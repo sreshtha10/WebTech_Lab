8473739359228957feebaae9cdb8e3839985c701
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-04-23 15:30:23 +0530

    Update app.js

--- 199301229 Sreshtha Mehrotra Caculator Using JavaScript/app.js
+++ 199301229 Sreshtha Mehrotra Caculator Using JavaScript/app.js
@@ -1,1 +1,12 @@
-// js file
+const numbers = document.getElementsByClassName("number");
+//console.log(numbers);
+
+Array.from(numbers).forEach(function(number){
+    number.addEventListener("click",numberClicked);
+});
+
+function numberClicked(e){
+    //console.log(e.target.textContent);
+    const label = document.getElementById("resultValue");
+    label.innerHTML = e.target.textContent ;
+}
